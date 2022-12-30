e090991de689e6d13f80d2583f8ffffa9a01b2cc
Jasmine Ulloa <ulloajazz6@gmail.com>
2022-12-29 20:57:46 -0800

    declared variables and created elements

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("linked!")
+
+var body = document.body;
+var div = document.createElement("div");
+var h1 = document.createElement("h1");
+var para1 = document.createElement("p");
+var para2 = document.createElement("p")
+
+var start = document.createElement("button")
+
