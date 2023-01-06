b527bf7b1ae9d986cc7327c6e52bbaea6b97ad9e
Jasmine Ulloa <ulloajazz6@gmail.com>
2023-01-05 22:21:34 -0800

    trying to added each choice from array object to the html

--- script.js
+++ script.js
@@ -6,19 +6,50 @@ var startScreen = document.querySelector(".start-screen");
 var quizScreen = document.querySelector(".quiz-screen");
 var scoreScreen = document.querySelector(".score-screen");
 var showQuestion = document.querySelector("#question")
-
-var secondsLeft = 5;
+var showChoices = document.querySelector("#choices")
+var showChoice1 = document.querySelector("#choice1")
+var showScore = document.querySelector("#score")
+var secondsLeft = 55;
 var counter = 0
 var currentIndex= 0;
 var timer;
 var quiz = [
+        {question: "Where do the script tags belong in the html?",
+        choices: ["Head", "Title", "Div", "Body"],
+        answer: "Body"},
+
         {question: "How many primative data types are there?",
         choices:["four", "five", "six", "seven" ] ,
-        answer: "seven" },
+        answer: "seven"},
 
-        {question: "Can you see this?",
-        choices: ["Yes", "No", "maybe", "idk"],
-        answer: 0 },
+        {question: "Which is NOT a click event?",
+        choices: ["onMouseOver", "onUnload", "onClick", "onPush"],
+        answer: "onPush" },
+
+        {question: "Which is a composite data type?",
+        choices: ["Boolean", "String", "Object", "Number"],
+        answer: "Object" },
+
+        {question: "Which is NOT an error type",
+        choices: ["Load time error", "Run time error", "Null error", "Logical Error"],
+        answer: "Null error" },
+
+        {question: "What is a primative data type?",
+        choices: ["Null", "String", "Object", "Array"],
+        answer: "String" },
+
+        {question: "What is an anonymous function?",
+        choices: ["A function with no variable", "A function with no code to run", "A function that has no name", "A function inside a for loop"],
+        answer: "A function that has no name" },
+
+        {question: "What symbol is used for making comments?",
+        choices: ["(", "/", "#", "!"],
+        answer: "/" },
+
+        {question: "What is .this keyword in JavaScript??",
+        choices: ["used as a variable", "refers to the object", "refers to the method", "Ar"],
+        answer: "refers to the object" },
+
 ]
 
 
@@ -35,30 +66,6 @@ function startQuiz(){
         }
     }, 1000)
 
-    // var h1 = document.createElement("h1")
-    // var choiceA = document.createElement("button")
-    // choiceA.setAttribute('id','1')
-    // var choiceB = document.createElement("button")
-    // choiceB.setAttribute('id','2')
-    // var choiceC = document.createElement("button")
-    // choiceC.setAttribute('id','3')
-    // var choiceD = document.createElement("button")
-    // choiceD.setAttribute('id','4')
-
-    // h1.innerText = quiz[0].question
-    // choiceA.textContent = "1. " + quiz[0].choiceA
-    // choiceB.textContent = "2. " + quiz[0].choiceB
-    // choiceC.textContent = "3. " + quiz[0].choiceC
-    // choiceD.textContent = "4. " + quiz[0].choiceD
-
-    // div.appendChild(h1)
-    // div.appendChild(choiceA)
-    // div.appendChild(choiceB)
-    // div.appendChild(choiceC)
-    // div.appendChild(choiceD)
-
-
-    
     renderQuestions();
 }
 
@@ -66,21 +73,28 @@ function renderQuestions() {
     currentQuestion = quiz[currentIndex];
     showQuestion.textContent = currentQuestion.question
 
-    currentQuestion.answer.forEach(element => {
-        
+    
+    currentQuestion.choices.forEach(choice => {
+        showChoices.textContent = choice
+        console.log(choice)
     });
+
+    showChoices.addEventListener("click", evaulateQuestion)
+   
 }
 
 
-
-function evaulateQuestion (){
-  
-    currentIndex
+function evaulateQuestion (e){
+    //set a variable for correctAnswer
+    // var correctAnswer = currentQuestion.answer
+    // correctAnswer ? score++ : secondsLeft-10 
+    // currentIndex
     console.log("i was clicked")
 }
 
 function endQuiz() {
     clearInterval(timer);
+    
     scoreScreen.classList.remove("hide");
     quizScreen.classList.add("hide");
 }
@@ -89,7 +103,7 @@ start.addEventListener("click", startQuiz)
 /*
 to do:
 
-✅ 1: create an object or array that has questions, choices and answers for the quiz
+1: create an object or array that has questions, choices and answers for the quiz
 2. iterate through the array and display each question and choices in the correct html element (p tag/ button")
 3. if the users choice === answer, display "correct"
 4. if the users choice does not equal answer , display "wrong" and minus 10 seconds from the timer
